8da63d4fecac765019dcbd97d48cd392f804ab4f
trpetr <103998970+trpetr@users.noreply.github.com>
2026-05-07 17:24:04 +0300

    added EnemyFP

---
+++ Assets/Prefabs/FirstPerson/Scripts/EnemyFP.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyFP : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private PlayerFP targetPlayer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Ищем игрока по компоненту PlayerFP
+        targetPlayer = Object.FindFirstObjectByType<PlayerFP>();
+    }
+
+    void Update()
+    {
+        // Проверка: есть ли игрок и стоит ли враг на навигационной сетке
+        if (targetPlayer != null && agent.isOnNavMesh)
+        {
+            // Просто обновляем точку назначения на текущую позицию игрока
+            agent.SetDestination(targetPlayer.transform.position);
+        }
+    }
+}
